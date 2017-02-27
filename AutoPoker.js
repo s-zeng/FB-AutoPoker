77d1a79c762ad9e63eef5172a1cce517639668f3
Simon <simon.zeng.68@gmail.com>
2017-02-26 20:41:01 -0500

    Create AutoPoker.js

---
+++ AutoPoker.js
@@ -0,0 +1,9 @@
+function autoPoke() {
+    var thing = document.getElementsByTagName("a");
+    for (var i = 0; i < thing.length; i++) {
+        if (thing[i].innerHTML.contains("Poke Back")) {
+            thing[i].click();
+        }
+    }
+    setTimeout(autoPoke, 1000);
+}
